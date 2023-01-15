ae91e03d8e6074576f4dfeac85beedc239c32543
muazmusliu <mm29197@seeu.edu.mk>
2023-01-15 13:37:51 +0100

    Initial commit

---
+++ src/main/java/system/warehouse/managment/ManagmentApplication.java
@@ -0,0 +1,13 @@
+package system.warehouse.managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagmentApplication.class, args);
+	}
+
+}

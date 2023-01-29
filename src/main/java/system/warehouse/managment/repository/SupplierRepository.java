17258f3248da9c4dfba29bab543d99537dcbb8f1
Alban <alban.abduli@outlook.com>
2023-01-30 00:11:56 +0100

    Created DefaultServices to make methods work!

--- src/main/java/system/warehouse/managment/repository/SupplierRepository.java
+++ src/main/java/system/warehouse/managment/repository/SupplierRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 @Repository
 public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
     List<Supplier> findAll();
-    Optional<Supplier> findById(Integer id);
+    Supplier findSupplierById(Integer id);
 }
